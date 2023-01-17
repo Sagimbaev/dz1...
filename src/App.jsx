ae83d3b15271267a72aefc572d9581eab156d24c
hamdamovvv <akhmadkhamdamov000@gmail.com>
2023-01-17 14:09:57 +0600

    HW1_Ahmad_20-03

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from "./components/Header/Header.jsx";
+import Intro from "./components/Intro/Intro.jsx";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Intro/>
+    </div>
+  )
+}
+
+export default App
